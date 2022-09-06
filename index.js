751c5476d6d7d976442814aa5a202a3cd59101b1
James Soares-Correia <jsoaresc@my.centennialcollege.ca>
2022-09-06 16:03:36 -0400

    navbar

--- index.js
+++ index.js
@@ -3,8 +3,11 @@ const express = require("express");
 const app = express();
 app.use(express.static('public'));
 
+app.set('view engine', 'ejs');
+
+
 app.get("/", function(req, res){
-    res.sendFile("index.html");
+    res.render("pages/index");
 });
 app.listen(3000, function(){
     console.log("Server started at 3000");
--- public/js/script.js
+++ public/js/script.js
@@ -1,1 +1,21 @@
-console.log("hello");
+const hamburger = $("#menuToggle");
+const mobileMenu = $("#menuItems");
+
+
+
+hamburger.on("click", function(){
+   if (hamburger.hasClass('open')){
+    
+    hamburger.removeClass('open'); 
+    mobileMenu.slideToggle();
+    setTimeout(function(){
+        $('body').removeClass('overflow-hidden');}, 300);
+    
+   }else{
+    $('body').addClass('overflow-hidden')
+    hamburger.addClass('open'); 
+    mobileMenu.slideToggle();
+   }
+    
+});
+
