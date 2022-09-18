7914dd258ff68bbcd23562078b114a533f5f7255
James Soares-Correia <jsoaresc@my.centennialcollege.ca>
2022-09-18 14:51:16 -0400

    proc

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ app.set('view engine', 'ejs');
 app.get("/", function(req, res){
     res.render("pages/index");
 });
-app.listen(3000, function(){
+
+app.listen(process.env.PORT || 3000, function(){
     console.log("Server started at 3000");
 });
